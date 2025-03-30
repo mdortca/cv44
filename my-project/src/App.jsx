1e9375af5086ca4bb00a818f0430ebec591c5ea4
Sdtshn <sdtsahin95@gmail.com>
2025-03-30 11:18:40 +0300

    toastfy kurulumu

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ToastContainer, toast } from 'react-toastify'; 
 
 function App() {
   const [count, setCount] = useState(0)
+  const notify = () => toast('Wow so easy !');
 
   return (
     <>
       <div>
+      <div className="">
+        <button onClick={notify}>Notify !</button>
+        <ToastContainer />
+      </div>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
